242ed69b6ca3ef0c28b3b04cf4976e6ff1f8e3b6
Achreftun <elmouelhi.achref@gmail.com>
2024-04-17 16:37:24 +0200

    fixing some warnings

--- src/components/MenuComponent.vue
+++ src/components/MenuComponent.vue
@@ -8,7 +8,7 @@
             <li><router-link to="/reactive">Reactive</router-link></li> |
             <li><router-link to="/adresse">Adresse</router-link></li> |
             <li><router-link to="/personne">Personne</router-link></li> |
-            <li><router-link :class="{ 'lien-actif': $route.name == 'tableau' }" to="/tableau">Tableau</router-link></li> |
+            <li><router-link :class="{ 'lien-actif': $route.name === 'tableau' }" to="/tableau">Tableau</router-link></li> |
           <li><router-link to="/compteur">Compteur</router-link></li>
         </ul>
     </nav>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -30,10 +30,3 @@ onMounted(() => {
 })
 
 </script>
-<style scoped>
-.logo {
-  height: 6em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-</style>
