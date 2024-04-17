8fec5fc787ac0aae1fcd44f90f4eeeb0c6ac606e
Achreftun <elmouelhi.achref@gmail.com>
2024-04-17 10:02:10 +0200

    adding some validators

--- src/components/PersonneAdd.vue
+++ src/components/PersonneAdd.vue
@@ -1,12 +1,49 @@
 <template>
-    <h1>Ajouter une nouvelle personne</h1>
+  <h2>Ajouter une nouvelle personne</h2>
+  <Form @submit="ajouterPersonne">
     <div>
-
+      <label for="nom">Nom</label>
+      <Field id="nom" name="nom" :rules="validateNom" />
+      <ErrorMessage  name="nom" />
     </div>
+    <div>
+      <label for="prenom">Prénom</label>
+      <Field id="prenom" name="prenom"  :rules="validatePrenom" />
+      <ErrorMessage  name="prenom" />
+    </div>
+    <div>
+      <label for="age">Âge</label>
+      <Field id="age" name="age"  :rules="'minMax:18,120'" />
+      <ErrorMessage  name="age" />
+    </div>
+    <div>
+      <button>Ajouter</button>
+    </div>
+  </Form>
 </template>
 <script>
+import {Field, Form, ErrorMessage} from "vee-validate";
+import * as yup from  'yup'
 export default {
-
+  components: {
+    Field, Form, ErrorMessage
+  },
+  data() {
+    return {
+      validateNom: yup
+          .string()
+          .required()
+          .matches(/^[A-Z]{1}.*/, 'Ce champ doit commencer par une majuscule'),
+      validateAge: yup.number().required().min(1).max(150),
+      validatePrenom: yup.string().required().matches(/^[A-Z]{1}.*/)
+    }
+  },
+  methods: {
+    ajouterPersonne(values, actions) {
+      console.log(values);
+      actions.resetForm()
+    }
+  }
 }
 </script>
 <style scoped></style>
--- src/views/PersonneView.vue
+++ src/views/PersonneView.vue
@@ -1,6 +1,7 @@
 <template>
     <h1>Gestion de personnes</h1>
     <PersonneAdd/>
+    <h2>Liste de personnes </h2>
     <ul>
         <li v-for='elt in personnes'>
         {{ elt.prenom }} {{ elt.nom }}
